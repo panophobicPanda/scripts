9e9076190a33d4dbe7241725dec527b3f7c0cd63
brian nathan smith <bsmith@the408.com>
2015-02-22 12:44:26 -0800

    script for cloning a bunch of machines via the vmware API

--- cloneAllqgpcp.sh
+++ cloneAllqgpcp.sh
@@ -1,6 +1,6 @@
-Relocating the virtual machine jdc01.qgpcp.qualys.com-
+#!/bin/bash
 # this machine will clone and migrate all VMs from the answer file vmNames.txt
-# Brian Smith Qualys Inc 2012
+# bsmith@the408.com 2012
 #
 API_User=bsmith
 API_Password=
@@ -8,14 +8,14 @@ URL=https://10.60.1.220/sdk/vimService
 answerFile=./vmNames.txt
 #
 #
-/usr/lib/vmware-vcli/apps/general/vidiscovery_modified.pl  --username $API_User --password $API_Password --url $URL --managedentity host --entityname qscapvm7.ops.qualys.com > ./vmNames.txt
+/usr/lib/vmware-vcli/apps/general/vidiscovery_modified.pl  --username $API_User --password $API_Password --url $URL --managedentity host --entityname qscapvm7.ops.example.com > ./vmNames.txt
 
 for VMNAME in `cat $answerFile`
 do
 #
-/usr/lib/vmware-vcli/apps/vm/vmclone.pl --username $API_User --password $API_Password --url $URL --vmhost qscapvm7.ops.qualys.com --vmname $VMNAME --vmname_destination "$VMNAME-" --datastore qscapvm7:datastore1
+/usr/lib/vmware-vcli/apps/vm/vmclone.pl --username $API_User --password $API_Password --url $URL --vmhost qscapvm7.ops.example.com --vmname $VMNAME --vmname_destination "$VMNAME-" --datastore qscapvm7:datastore1
 #
-/usr/lib/vmware-vcli/apps/vm/vmmigrate.pl --username $API_User --password $API_Password --url $URL --sourcehost qscapvm7.ops.qualys.com --targetdatastore qscapvm8:datastore1 --targethost qscapvm8.ops.qualys.com --targetpool qgpcp-v1.0-copy1 --vmname "$VMNAME-"
+/usr/lib/vmware-vcli/apps/vm/vmmigrate.pl --username $API_User --password $API_Password --url $URL --sourcehost qscapvm7.ops.example.com --targetdatastore qscapvm8:datastore1 --targethost qscapvm8.ops.example.com --targetpool qgpcp-v1.0-copy1 --vmname "$VMNAME-"
 
 done
 exit
