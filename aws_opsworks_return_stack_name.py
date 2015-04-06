115795b114ced2afdb853adc75b08e2ffba47918
brian nathan smith <bsmith@the408.com>
2015-04-06 12:11:18 -0700

    adding

---
+++ aws_opsworks_return_stack_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#brian.smith@the408.com 2015-04-06
+
+import json
+import glob
+import os
+#from pprint import pprint
+
+newest = max(glob.iglob('/var/lib/aws/opsworks/chef/*.json'), key=os.path.getctime)
+
+with open(newest) as data_file:
+        data = json.load(data_file)
+print data[u'opsworks'][u'stack'][u'name']
+
+#use the following line to print all json values
+#pprint(data)
