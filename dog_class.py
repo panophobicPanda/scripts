dd8831022d521bb0e0d56049685eebf2d413ed51
brian smith <brian.smith@healthfidelity.com>
2015-06-21 09:47:18 -0700

    initial commit

---
+++ dog_class.py
@@ -0,0 +1,8 @@
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
