dd08cc90447854ac11e99707309839bd5c5214c6
Brian Smith <bsmith01@tenable.com>
2016-03-01 11:53:14 -0800

    cleanup

--- aws_ec2_instance_info.py
+++ aws_ec2_instance_info.py
@@ -6,15 +6,13 @@
 from __future__ import print_function
 
 import boto.ec2
+import pprint
 
 def instances ( str ):
     conn = boto.ec2.connect_to_region("us-east-1", profile_name = str)
     instances = conn.get_only_instances()
     for instance in instances:
-        if 'Name' in instance.tags:
-                print(instance.instance_type, instance.state, instance.id, instance.tags['Name'], end="")
-        else:
-                print(instance.instance_type, instance.state, instance.id, end="")
+        print(instance.private_ip_address, instance.ip_address, instance.instance_type, instance.state, instance.id, instance.tags, end="")
         for group in instance.groups:
                 print(" "+group.id, end="")
 	print("")
