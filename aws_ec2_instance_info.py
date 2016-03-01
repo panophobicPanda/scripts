fb43ceeb73b05c01a5e0138367a4d97fb5ccfa6d
Brian Smith <bsmith01@tenable.com>
2016-03-01 11:34:36 -0800

    fixing security group formatting

--- aws_ec2_instance_info.py
+++ aws_ec2_instance_info.py
@@ -3,18 +3,21 @@
 #https://github.com/akabdog/scripts
 
 
+from __future__ import print_function
+
 import boto.ec2
 
 def instances ( str ):
-    conn = boto.ec2.connect_to_region("us-west-1", profile_name = str)
+    conn = boto.ec2.connect_to_region("us-east-1", profile_name = str)
     instances = conn.get_only_instances()
     for instance in instances:
         if 'Name' in instance.tags:
-                print "%s %s %s %s" % (instance.instance_type, instance.state, instance.id, instance.tags['Name'])
+                print(instance.instance_type, instance.state, instance.id, instance.tags['Name'], end="")
         else:
-                print "%s %s %s" % (instance.instance_type, instance.state, instance.id)
+                print(instance.instance_type, instance.state, instance.id, end="")
         for group in instance.groups:
-                print(group.id)
+                print(" "+group.id, end="")
+	print("")
 #
 # MAIN
 #
