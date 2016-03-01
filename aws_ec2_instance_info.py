b65154122e966f66266db49fc2686967ea621d14
Brian Smith <bsmith01@tenable.com>
2016-03-01 11:18:16 -0800

    adding sec groups

--- aws_ec2_instance_info.py
+++ aws_ec2_instance_info.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
-#bsmith@the408.com  2015-04-06 
+#bsmith@the408.com  2015-04-06
 #https://github.com/akabdog/scripts
 
 
 import boto.ec2
- 
+
 def instances ( str ):
     conn = boto.ec2.connect_to_region("us-west-1", profile_name = str)
     instances = conn.get_only_instances()
     for instance in instances:
-	if 'Name' in instance.tags:
-		print "%s %s %s %s" % (instance.instance_type, instance.state, instance.id, instance.tags['Name'])
-	else:
-		print "%s %s %s" % (instance.instance_type, instance.state, instance.id)
- 
+        if 'Name' in instance.tags:
+                print "%s %s %s %s" % (instance.instance_type, instance.state, instance.id, instance.tags['Name'])
+        else:
+                print "%s %s %s" % (instance.instance_type, instance.state, instance.id)
+        for group in instance.groups:
+                print(group.id)
 #
 # MAIN
 #
-instances("aws-prod")
+instances("prod")
